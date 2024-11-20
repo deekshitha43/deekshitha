2c7f156209b4ae3bae457e3e67279e8489a19c02
deekshitha43 <deekshugowda229@gmail.com>
2024-11-20 08:19:04 +0530

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 08:15:10 2024
+
+@author: HP
+"""
+
+def calculate_sum(a, b):
+    total = a + b
+    return total
+
+x = 5
+y = 10
+z = calculate_sum(x, w)
+print(z)
